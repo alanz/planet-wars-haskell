d8564508adca83e714fc294218f2a4043ddbc582
alanz <alanz@alanz-laptop.(none)>
2010-09-22 21:53:20 +0200

    Added example_bots jar files from java starter kit, made a script to run the thing (also testing that I can commit)

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Run a bot against the standard one
+
+java -jar tools/PlayGame.jar maps/map7.txt 1000 1000 log.txt \
+        "./MyBot" "java -jar example_bots/RandomBot.jar" | \
+        java -jar tools/ShowGame.jar
+
